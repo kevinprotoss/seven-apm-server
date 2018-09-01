e22c0509916f345bc716a918554d3ee3fe15ce2d
Junxiang Wei <kevinprotoss.wei@googlemail.com>
2018-09-01 21:45:47 +0000

    update all

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const Koa = require('koa');
+const compress = require('koa-compress');
+const conditional = require('koa-conditional-get');
+const etag = require('koa-etag');
 const serve = require('koa-static');
 const app = new Koa();
 
+app.use(compress());
+app.use(conditional());
+app.use(etag());
 app.use(serve('public'));
-app.listen(8080, '0.0.0.0');
+app.listen(process.env.PORT, process.env.IP);
