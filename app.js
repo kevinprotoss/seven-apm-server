2f2c841db779bde0cacf257f743472c2f17dfaa1
Junxiang Wei <kevinprotoss.wei@googlemail.com>
2018-09-03 21:54:29 +0000

    enhance with error torance for angular app

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 'use strict';
 
 const Koa = require('koa');
+const Router = require('koa-router');
 const compress = require('koa-compress');
 const conditional = require('koa-conditional-get');
 const etag = require('koa-etag');
 const serve = require('koa-static');
+const send = require('koa-send');
 const app = new Koa();
+const router = new Router();
 
-app.use(compress());
-app.use(conditional());
-app.use(etag());
-app.use(serve('public'));
+router
+  .get('*', async (ctx) => {
+    // Fallback route for 404 error while refreshing angular app with hash
+    await send(ctx, 'public/index.html');
+  });
+
+app
+  .use(compress())
+  .use(conditional())
+  .use(etag())
+  .use(serve('public'))
+  .use(router.routes())
+  .use(router.allowedMethods());
+
 app.listen(process.env.PORT, process.env.IP);
